a9cc9a65e3ff169303a40c8d6758114eee120bca
JacksonByrd7 <jackson.byrd7@gmail.com>
2024-04-02 16:27:25 -0400

    added mysql2, took new approach with server.js

--- server.js
+++ server.js
@@ -1,29 +1,28 @@
-const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
-//connect to the database
-const db = mysql.createConnection(
-    {
-        host: "localhost",
-        user: "root",
-        password: "",
-        database: "staff_db",
-    },
-    console.log("Successfully connected to database.")
-);
+// Connect to the database
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "staff_db",
+}, console.log("Successfully connected to database."));
 
-//prompt for inquirer
-const prompt = inquirer.createPromptModule();
+// Prompt for inquirer
+const prompt = async () => {
+    const { default: inquirer } = await import("inquirer");
+    return inquirer;
+};
 
 const saveEmployees = (answer) => {
     db.promise().query("INSERT INTO employee SET ?", answer)
-    .then((result) => {
-        console.log("Adding new employee")
-        init();
-    });
+        .then((result) => {
+            console.log("Adding new employee")
+            init();
+        });
 };
 
-// questions for employees and roles
+// Questions for employees and roles
 const addEmployee = (roles, managers) => {
     const questions = [
         {
@@ -38,7 +37,7 @@ const addEmployee = (roles, managers) => {
         },
         {
             type: "input",
-            list: "role_id",
+            name: "role_id",
             message: "What is the employee's role?",
             choices: roles,
         },
@@ -50,128 +49,38 @@ const addEmployee = (roles, managers) => {
         }
     ];
 
-    inquirer.prompt(questions).then(saveEmployees);
+    prompt().then((inquirer) => {
+        inquirer.prompt(questions).then(saveEmployees);
+    });
 }
 
-// funciton to start the program
+// Function to start the program
 const init = () => {
-    prompt({
-      type: "rawlist",
-      message: "What would you like to do?",
-      name: "view",
-      choices: [
-        "View all departments",
-        "View all roles",
-        "View all employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Exit"
-      ],
-    }).then((answer) => {
-      if (answer.view === "View all departments") {
-        db.query("SELECT * FROM department", (error, departments) => {
-          if (error) console.error(error);
-          console.table(departments);
-          init();
-        });
-      }
-      if (answer.view === "View all roles") {
-        db.query("SELECT * FROM role", (error, roles) => {
-          if (error) console.error(error);
-          console.table(roles);
-          init();
-        });
-      }
-      if (answer.view === "View all employees") {
-        db.query("SELECT * FROM employee", (error, employees) => {
-          if (error) console.error(error);
-          console.table(employees);
-          init();
-        });
-      }
-  
-      if (answer.view === "Exit") {
-        process.exit();
-        init();
-      }
-  
-  
-      if (answer.view === "Add a department") {
+    prompt().then((inquirer) => {
         inquirer.prompt({
-          type: "input",
-          name: "department",
-          message: "What is the name of the department?",
-        })
-          .then((answer) => {
-            const { department } = answer;
-  
-            db.promise().query("INSERT INTO department (name) VALUE(?) ", department)
-              .then((answer) => console.log("Adding new department"));
-            init();
-          });
-      }
-  
-  
-      if (answer.view === "Add a role") {
-        db.promise()
-          .query("SELECT id, name FROM department")
-          .then(([answer]) => {
-            const deptArr = answer.map((dept) => ({
-              name: dept.name,
-              value: dept.id,
-            }));
-            inquirer
-              .prompt([
-                {
-                  type: "input",
-                  name: "title",
-                  message: "What is the name of the role?",
-                },
-                {
-                  type: "input",
-                  name: "salary",
-                  message: "What is the salary?",
-                },
-  
-                {
-                  type: "list",
-                  name: "department_id",
-                  message: "What is the department?",
-                  choices: deptArr,
-                },
-              ])
-              .then((answer) => {
-                db.promise()
-                  .query("INSERT INTO role SET ?", answer)
-                  .then(() => {
-                    console.log("Adding new role");
+            type: "rawlist",
+            message: "What would you like to do?",
+            name: "view",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Exit"
+            ],
+        }).then((answer) => {
+            if (answer.view === "View all departments") {
+                db.query("SELECT * FROM department", (error, departments) => {
+                    if (error) console.error(error);
+                    console.table(departments);
                     init();
-                  })
-                  .catch((err) => console.error(err));
-              });
-          });
-      }
-  
-      if (answer.view === "Add an employee") {
-        db.promise().query("SELECT id, title FROM role")
-          .then(([roles]) => {
-            db.promise().query("SELECT id, first_name, last_name FROM employee")
-              .then(([employees]) => {
-                const roleChoices = roles.map((role) => ({
-                  name: role.title,
-                  value: role.id,
-                }));
-                const managerChoices = employees.map((mgr) => ({
-                  name: mgr.first_name + ' ' + mgr.last_name,
-                  value: mgr.id,
-                }));
-                addAnEmployee(roleChoices, managerChoices);
-              });
-          });
-  
-      };
+                });
+            }
+            // Add other conditions here...
+        });
     });
-  }
-  
-  init();
+}
+
+init();
